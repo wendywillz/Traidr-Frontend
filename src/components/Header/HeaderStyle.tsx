80fe074459f322b99a61cc01529ccca77e192cb8
abdrasaq14 <abdrasaq14@gmail.com>
2024-02-22 05:10:24 +0100

    landing-page-design

---
+++ src/components/Header/HeaderStyle.tsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+const HeaderStyle = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0rem 2rem;
+  height: 4rem;
+  margin-bottom: 2rem;
+
+  .header-inner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-basis: 100%;
+    width: 100%;
+    @media (min-width: 1500px) {
+      max-width: 1500px;
+    }
+  }
+  .logo-wrapper {
+    flex-basis: 60%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+  .traidr-header-logo {
+    width: 6rem;
+  }
+  .header-right-btn-wrapper {
+    flex-basis: 40%;
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    justify-content: flex-end;
+  }
+  .small-screen-icon {
+    display: block;
+    color: var(--orange-color);
+    @media (min-width: 768px) {
+      display: none;
+    }
+  }
+  .big-screen {
+    @media (max-width: 768px) {
+      display: none;
+    }
+  }
+  .header-right-login-btn {
+    background-color: #fff;
+    color: var(--orange-color);
+    border: none;
+    padding: 0.8rem 2rem;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  .header-right-signup-btn {
+    background-color: var(--orange-color);
+    color: white;
+    border: none;
+    padding: 0.8rem 2rem;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+`;
+
+export default HeaderStyle;
---
+++ src/views/LandingPage/LandingPageStye.tsx
@@ -0,0 +1,168 @@
+import styled from "styled-components";
+
+export const LandingPageMainWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0rem 2rem;
+  min-height: 80vh;
+
+  .landing-page-inner-body-wrapper {
+    display: flex;
+    gap: 2rem;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 1500px) {
+      max-width: 1500px;
+    }
+  }
+  .landing-page-first-section-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    margin: 2rem 0;
+    @media (min-width: 768px) {
+      flex-direction: row;
+      gap: 2rem;
+      justify-content: space-between;
+    }
+  }
+  .landing-page-left-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+    text-align: center;
+    @media (min-width: 768px) {
+      flex-basis: 50%;
+      text-align: left;
+    }
+  }
+  .landing-page-left-wrapper h1 {
+    span {
+      color: var(--orange-color);
+    }
+  }
+  .landing-page-left-wrapper p {
+    color: var(--normal-text-color);
+  }
+  .input-search-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border: 1px solid var(--normal-text-color);
+    border-radius: 4px;
+    height: 3rem;
+    width: 100%;
+  }
+  .input-search-wrapper input {
+    border: none;
+    outline: none;
+    padding: 0.8rem 1rem;
+    flex-basis: 70%;
+    &::placeholder {
+      color: var(--input-placeholder-color);
+    }
+    &:focus {
+      border: none;
+    }
+  }
+  .input-search-wrapper button {
+    background-color: var(--orange-color);
+    color: white;
+    flex-basis: 30%;
+    height: 100%;
+    width: 100%;
+    border: none;
+    padding: 0.8rem 2rem;
+    cursor: pointer;
+  }
+  .landing-page-right-wrapper {
+    justify-content: center;
+    align-items: center;
+    display: none;
+    @media (min-width: 768px) {
+      display: flex;
+      flex-basis: 50%;
+    }
+  }
+  .landing-page-right-wrapper img {
+    width: 100%;
+    object-fit: cover;
+  }
+
+  //   second section
+  .landing-page-second-section-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    margin: 2rem 0;
+    @media (min-width: 768px) {
+      flex-direction: row;
+      gap: 2rem;
+      justify-content: space-between;
+      align-items: stretch;
+    }
+  }
+  .landing-page-second-section-left-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+    text-align: center;
+    @media (min-width: 768px) {
+      flex-basis: 45%;
+      text-align: left;
+    }
+  }
+  .landing-page-second-section-left-wrapper h1 {
+    display: flex;
+    align-items: stretch;
+    justify-content: space-between;
+  }
+  .landing-page-second-section-left-wrapper h1 span {
+    display: flex;
+    align-items: center;
+    justify-content: stretch;
+    width: 4.5rem;
+    margin-left: 0.5rem;
+    margin-right: 4rem;
+    img {
+      margin-top: 5px;
+      height: 200%;
+      object-fit: cover;
+    }
+  }
+  .why-use-traidr-wrapper {
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: repeat(2, 1fr);
+  }
+  .each-why-use {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: flex-start;
+    justify-content: flex-start;
+  }
+  .why-use-image-top img {
+    width: 3rem;
+  }
+  .why-use-text p {
+    color: var(--normal-text-color);
+    text-align: left;
+  }
+  .landing-page-second-section-right-wrapper {
+    display: none;
+    @media (min-width: 768px) {
+      display: flex;
+      flex-basis: 55%;
+    }
+  }
+  .landing-page-second-section-right-wrapper img {
+    width: 100%;
+    object-fit: cover;
+  }
+`;
