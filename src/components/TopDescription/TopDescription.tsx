7513ee21607af17cc4cbf9e121506b894b02ba6d
Kofoworola <kofoworolao11@gmail.com>
2024-03-12 18:05:32 +0100

    dummy-descriptiom

--- src/components/TopDescription/TopDescription.tsx
+++ src/components/TopDescription/TopDescription.tsx
@@ -1,26 +1,28 @@
 import { images, number } from "./index";
 import { IoIosTimer } from "react-icons/io";
 import { FaLocationDot } from "react-icons/fa6";
+import { useState } from "react";
 import "./TopDescription.css";
+import { Link } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
+import { MdOutlineChat } from "react-icons/md";
 
 function TopDescription() {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleSelectChange = (e: any) => {
+    setSelectedOption(e.target.value);
+  };
+
   return (
     <div>
       <section className="display">
         <div>
           <ul className="home-search">
-            <li>
-              <a href="">Home /</a>
-            </li>
-            <li>
-              <a href="">search /</a>
-            </li>
-            <li>
-              <a href="">Home appliances /</a>
-            </li>
-            <li>
-              <a href="">Hans Red Blender set</a>
-            </li>
+            <Link to="">Home /</Link>
+            <Link to="">Search /</Link>
+            <Link to="">Home appliances</Link>
+            <Link to="">Home appliances</Link>
           </ul>
 
           <div className="img-container">
@@ -32,31 +34,72 @@ function TopDescription() {
           </div>
         </div>
 
-        <div>
+        <div className="sec-section">
           <div className="views">
             <div>
-              <p> 3 Views in the last 2 minutes </p>
-              <p>N30,000</p>
-              <p>Wallphy Store</p>
-              <p>Hans Blender Set</p>
+              <p className="views-time"> 3 Views in the last 2 minutes </p>
+              <h2 className="price">N30,000</h2>
+              <p className="store">Wallphy Store</p>
+              <p className="hens-product">Hans Blender Set</p>
             </div>
 
             <div>
-              <p>Promoted</p>
+              <div className="block">
+                <p className="promoted">Promoted</p>
 
-              <div>
-                <IoIosTimer />
-                <p>Posted 2 mins ago</p>
+                <div className="set-timer">
+                  <IoIosTimer width={30} height={30} />
+                  <p>Posted 2 mins ago</p>
+                </div>
+
+                <div className="set-location">
+                  <FaLocationDot width={30} height={30} />
+                  <p>Lagos, Gbagada</p>
+                </div>
               </div>
 
-              <div>
-                <FaLocationDot />
-                <p>Lagos, Gbagada</p>
+              <div className="color-selection">
+                <label htmlFor="">Select Colour</label>
+                <select
+                  className="set-color"
+                  value={selectedOption}
+                  onChange={handleSelectChange}
+                  name=""
+                  id=""
+                >
+                  <option value="">Select...</option>
+                  <option value="">Red</option>
+                  <option value="">Blue</option>
+                  <option value="">Yellow</option>
+                </select>
               </div>
             </div>
           </div>
 
-          <div></div>
+          <div className="sectwo">
+            <div className="sub-sec">
+              <p></p>
+              <Link className="wishlist" to="">
+                Add to Wishlist
+              </Link>
+            </div>
+
+            <div className="user-info">
+              <FaUserCircle style={{ width: "7rem", height: "7rem" }} />
+
+              <div className="data-group">
+                <h2>Bankole Yusuf</h2>
+                <p>BankoleYusuf@gmail.com</p>
+                <div className="chat-group">
+                  <MdOutlineChat
+                    style={{ width: "1rem", height: "1rem", color: "#219653" }}
+                  />
+                  <p>start chat</p>
+                </div>
+                <p>contact: 09085757757,08065443355</p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
