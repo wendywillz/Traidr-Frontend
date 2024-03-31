9a5e07add22a1a4bc7a32504507ab84beedfb8e6
abdrasaq14 <abdrasaq14@gmail.com>
2024-03-31 15:07:53 +0100

    average usage time

--- src/components/AverageUsageTime/AverageUSageTime.tsx
+++ src/components/AverageUsageTime/AverageUSageTime.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { ReactNode, useEffect } from "react";
 import axiosInstance from "../../utils/axiosInstance";
 import { useSelector } from "react-redux";
