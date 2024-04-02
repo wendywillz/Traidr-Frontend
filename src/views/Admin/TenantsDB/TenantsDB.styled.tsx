8dda4496f0a69fd6f90b1354037063825ed62d67
wendywillz <jolahwilliams215@gmail.com>
2024-04-02 09:36:49 +0100

    tenantdb

---
+++ src/views/Admin/TenantsDB/TenantsDB.styled.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const TenantsDBContainer = styled.div`
+    
+width: 100%;
+
+display: flex;
+flex-direction: column;
+
+justify-content: center;
+
+.table-container {
+    margin: 20px;
+  }
+  
+  table {
+    width: 70%;
+    border-collapse: collapse;
+    height: 90vh;
+    
+  }
+  
+  th, td {
+    padding: 10px;
+    text-align: left;
+  }
+  
+  th {
+    background-color: #f2f2f2;
+  }
+  
+  td input {
+    width: 100%;
+    border: none;
+    padding: 5px;
+    outline: none;
+  }
+  `
