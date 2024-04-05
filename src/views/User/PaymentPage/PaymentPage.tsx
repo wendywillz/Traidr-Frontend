b5fe3b792d7b60f89612a1a607f9ad2477119149
Elephant Girl <elephantgirlworks@gmail.com>
2024-04-05 08:37:42 +0100

    Edits to Payment page

--- src/views/User/PaymentPage/PaymentPage.tsx
+++ src/views/User/PaymentPage/PaymentPage.tsx
@@ -4,14 +4,31 @@ import { PaymentPageWholeContainer, PaymentPageMainContainer, PaymentPageContent
 
 import Header from "../../../components/Header/Header"
 import MultipurposeModal from "../../../components/MultipurposeModal/MultipurposeModal"
+import { useState } from "react"
+import axiosInstance from "../../../utils/axiosInstance"
+import { useSelector } from "react-redux"
+import { RootState } from "../../../app/store"
+import { NavigateFunction, useNavigate } from "react-router-dom";
 
 const PaymentPage = () => {
+    const navigate:NavigateFunction = useNavigate();
+    const userId:string|null = useSelector((state: RootState)=> state.user.userId)
 
-
+  //HANDLING THE PROCEED TO Purchase MODAL
+  const [purchaseModalVisibility, setPurchaseModalVisibility] = useState(false)
+  const purchaseModalTitle = `PURCHASE COMPLETED`
+  const purchaseModalMessage = `Thank you for shopping with Traidr`
+  const purchaseModalButtonAction = ()=>{navigate('/dashboard')}
+  const togglePurchaseModal = ()=>{
+    setPurchaseModalVisibility(!purchaseModalVisibility)
+  }
 
 
   return (
     <PaymentPageWholeContainer>
+        {purchaseModalVisibility && <MultipurposeModal title={purchaseModalTitle} message={purchaseModalMessage} onClickAction={purchaseModalButtonAction}/>}
+
+
         <Header/>
         <PaymentPageMainContainer>
             <PaymentPageContentContainer>
@@ -22,7 +39,7 @@ const PaymentPage = () => {
                 <PaymentPageButtonsContainer>
                     <PaymentPageButton className="payment-page-cancel-button">CANCEL</PaymentPageButton>
 
-                    <PaymentPageButton className="payment-page-checkout-button">PAY</PaymentPageButton>
+                    <PaymentPageButton className="payment-page-checkout-button" onClick={togglePurchaseModal}>PAY</PaymentPageButton>
                 </PaymentPageButtonsContainer>
             </PaymentPageContentContainer>
         </PaymentPageMainContainer>
