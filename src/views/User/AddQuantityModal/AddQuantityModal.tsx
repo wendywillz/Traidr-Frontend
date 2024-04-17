dbec32eee0c399618028e7e32dab2f25054f1d6d
abdrasaq14 <abdrasaq14@gmail.com>
2024-04-17 12:37:37 +0100

    add to cart

--- src/views/User/AddQuantityModal/AddQuantityModal.tsx
+++ src/views/User/AddQuantityModal/AddQuantityModal.tsx
@@ -1,43 +1,114 @@
-import { ChangeEvent, useState } from "react"
-import { AddQuantityModalContainer,QuantityModalSection, QuantityCounterButton, ModalInputField, ModalCancelButton, QuantityModalButton} from "./AddQuantityModal.Styled"
+//style imports
+import {
+  AddQuantityModalContainer,
+  QuantityModalSection,
+  QuantityCounterButton,
+  ModalInputField,
+  ModalCancelButton,
+  QuantityModalButton,
+} from "./AddQuantityModal.Styled";
+
+//package imports
+import { ChangeEvent, useState, useEffect } from "react";
+import axiosInstance from "../../../utils/axiosInstance";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../../app/features/cart/cartSlice";
+interface CartDetails {
+  currentProductId: string | undefined | null;
+  productQuantity: number;
+}
 
 interface ModalProps {
-    toggleVisiblity: () => void;
-  }
+  productId: string | undefined | null;
+  toggleVisibility: () => void;
+  //     quantity: number;
+  //     increaseQuantity: ()=>void;
+  //     decreaseQuantity: ()=>void;
+  //     handleChange: ()=>void;
+  //     handleSubmit: (e: ChangeEvent)=>void;
+}
 
-const AddQuantityModal = ({toggleVisiblity}:ModalProps) => {
+const AddQuantityModal = ({ productId, toggleVisibility }: ModalProps) => {
+  const [quantity, setQuantity] = useState<number>(0);
+  const [cartData, setCartData] = useState<CartDetails>({
+    currentProductId: productId,
+    productQuantity: 0,
+  });
+  const dispatch = useDispatch();
+  // const [quantityModalVisibility, setQuantityModalVisibility] = useState(false);
 
-    const[inputValue, setInputValue] = useState(0)
+  //function to make modal visible
+  // const toggleVisibility = () => {
+  //   console.log("profile", quantityModalVisibility);
+  //   setQuantityModalVisibility(!quantityModalVisibility);
+  // };
 
-    const increaseQuantity = ()=>{
-        setInputValue(inputValue + 1)
+  //functions to change quantity with buttons and input field
+
+  useEffect(() => {
+    setCartData({ ...cartData, productQuantity: quantity });
+  }, [cartData, quantity]);
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
     }
+  };
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuantity(parseInt(event.target.value));
+  };
 
-    const decreaseQuantity = ()=>{
-        if(inputValue > 0) setInputValue(inputValue - 1)
-
+  //function to handle submit
+  const handleSubmit = async () => {
+    if (quantity > 0) {
+      // event.preventDefault()
+      try {
+        const res = await axiosInstance.post(`/cart/add-to-cart/`, cartData);
+        if (res && res.data.success) {
+          dispatch(addToCart({ quantity, type: "add" }));
+        }
+        // location.reload()
+      } catch (error) {
+        return error;
+      }
+      toggleVisibility();
+    } else {
+      toggleVisibility();
     }
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) =>{
-        setInputValue(parseInt(event.target.value))
-    }
-    const handleSubmit = ()=>{
-        toggleVisiblity()
-    }
+  };
 
   return (
     <AddQuantityModalContainer>
-        <ModalCancelButton onClick={toggleVisiblity}>x</ModalCancelButton>
-        <QuantityModalSection>
-            <QuantityCounterButton id="remove-button" onClick={decreaseQuantity}>-</QuantityCounterButton>
-            <ModalInputField >
-            <input type="number" id="quantityInput" name="productQuantity" value={inputValue} placeholder={inputValue.toString()} onChange={handleChange} className="quantity-modal-input"/>
-            </ModalInputField >
-            <QuantityCounterButton id="pos-button" onClick={increaseQuantity}>+</QuantityCounterButton>
-        </QuantityModalSection>
-        <QuantityModalButton onClick={handleSubmit}>ADD TO CART</QuantityModalButton>
-        
+      <ModalCancelButton onClick={toggleVisibility}>x</ModalCancelButton>
+      <QuantityModalSection>
+        <QuantityCounterButton id="remove-button" onClick={decreaseQuantity}>
+          -
+        </QuantityCounterButton>
+        <ModalInputField>
+          <input
+            type="number"
+            id="quantityInput"
+            name="productQuantity"
+            value={quantity}
+            placeholder={quantity.toString()}
+            onChange={handleChange}
+            className="quantity-modal-input"
+          />
+        </ModalInputField>
+        <QuantityCounterButton id="pos-button" onClick={increaseQuantity}>
+          +
+        </QuantityCounterButton>
+      </QuantityModalSection>
+      <QuantityModalButton onClick={handleSubmit}>
+        ADD TO CART
+      </QuantityModalButton>
     </AddQuantityModalContainer>
-  )
-} 
+  );
+};
 
-export default AddQuantityModal
+export default AddQuantityModal;
+
+// {quantity, toggleVisiblity, increaseQuantity, decreaseQuantity, handleChange, handleSubmit}:ModalProps
